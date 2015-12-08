f5127bdcd948dcce277b602fe5a35e45570c2ed7
CrazyGameInc <pavnovik@gmail.com>
2015-12-09 02:54:37 +0300

    Добавил комментарий

---
+++ app/src/main/java/ru/crazy/androidgit/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.crazy.androidgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // Денис привет
+    // LOL
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
